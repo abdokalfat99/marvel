1a8745c4fd6a44a6958f016e5176205848edaf7b
abdo-kalfat <152688775+abdo-kalfat@users.noreply.github.com>
2024-01-07 16:32:48 +0200

    quran

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class home_screen extends StatefulWidget {
+  const home_screen({super.key});
+
+  @override
+  State<home_screen> createState() => _home_screenState();
+}
+
+// ignore: camel_case_types
+class _home_screenState extends State<home_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
